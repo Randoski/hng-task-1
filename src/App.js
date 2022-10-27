980e9d73ea3dba1dfabcf641e74f4c4c72d8734b
Randy <monyerandy15@gmail.com>
2022-10-27 23:16:51 +0100

    Mobile design

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 // import react from 'react';
 import './App.css';
-import ProfileSection from './components/ProfileSection';
+import ProfileSection from './components/Profile-Section';
 import LinkSection from './components/LinkSection';
 import Footer from './components/Footer';
 
 function App() {
-  return <section>
+  return <sectionn id="page-container">
     <ProfileSection />
     <LinkSection />
     <Footer />
-  </section>
+  </sectionn>
 }
 
 export default App;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,14 @@
 import React from "react";
+import '../assets/styles/footer.css';
+
 
 function Footer() {
     return (
-        <section>
-            <div>
-                <img alt="Profile pics" src={require("../assets/images/zuri.jpg")} />
+        <section className="footer-container">
+            <div >
+                <img alt="Profile pics" src={require("../assets/images/zuri.jpg")} className="zuri-logo" />
                 <p>HNG Internship 9 Frontend Task</p>
-                <img alt="Profile pics" src={require("../assets/images/I4G.jpg")} />
+                <img alt="Profile pics" src={require("../assets/images/I4G.jpg")} className="i4g-logo" />
             </div>
         </section>
     );
